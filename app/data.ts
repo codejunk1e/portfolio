d852a2d0ce8789ea85c30f1ec1200a6d7ed796bc
Robin <emeka14real@gmail.com>
2025-09-30 20:21:13 +0100

    update basePath

--- app/data.ts
+++ app/data.ts
@@ -33,14 +33,14 @@ export const PROJECTS: Project[] = [
     description:
       'Fintech app for sending and reciving money from different currencies',
     link: 'https://play.google.com/store/apps/details?id=co.grey.mobile.android&hl=en&pli=1',
-    url: '/grey.png',
+    url: '${basePath}/grey.png',
     id: 'project1',
   },
   {
     name: 'JustEase',
     description: 'App to educate and enlightenment citizens about their rights and duties',
     link: 'https://play.google.com/store/apps/details?id=com.lawpavilion.lr&hl=en',
-    url: '/justease.png',
+    url: '${basePath}/justease.png',
     id: 'project2',
   },
 ]
