0f83ea214c58a5778bf2a0dcf04c864b4c834e6e
Robin <emeka14real@gmail.com>
2025-09-30 20:45:19 +0100

    Fix basePath not overidden

--- app/data.ts
+++ app/data.ts
@@ -27,7 +27,7 @@ type SocialLink = {
   link: string
 }
 
-const config = require('../next.config');
+import config from '../next.config'
 
 export const PROJECTS: Project[] = [
   {
@@ -35,14 +35,14 @@ export const PROJECTS: Project[] = [
     description:
       'Fintech app for sending and reciving money from different currencies',
     link: 'https://play.google.com/store/apps/details?id=co.grey.mobile.android&hl=en&pli=1',
-    url: '${config.basePath}/grey.png',
+    url: `${config.basePath}/grey.png`,
     id: 'project1',
   },
   {
     name: 'JustEase',
     description: 'App to educate and enlightenment citizens about their rights and duties',
     link: 'https://play.google.com/store/apps/details?id=com.lawpavilion.lr&hl=en',
-    url: '${config.basePath}/justease.png',
+    url: `${config.basePath}/justease.png`,
     id: 'project2',
   },
 ]
