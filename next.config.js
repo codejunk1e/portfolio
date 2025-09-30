ec614a1788ba01ba5103d33f1130751f828bad45
Igboanyika Nnaemeka Robin <emeka14real@gmail.com>
2025-09-30 17:01:26 +0100

    update base path

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
+const isProd = process.env.NODE_ENV === "production"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: isProd ? "/portfolio": "", 
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   output: 'export',
