28695fd140dca30b5569d823789610b2defbcf39
Robin <emeka14real@gmail.com>
2025-09-30 20:05:51 +0100

    Complete portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ import {
 } from '@/components/ui/morphing-dialog'
 import Link from 'next/link'
 import { AnimatedBackground } from '@/components/ui/animated-background'
+import { useState } from 'react';
+import { ProgressiveBlur } from '@/components/motion-primitives/progressive-blur';
+
 import {
   PROJECTS,
   WORK_EXPERIENCE,
@@ -43,51 +46,57 @@ type ProjectVideoProps = {
   src: string
 }
 
-function ProjectVideo({ src }: ProjectVideoProps) {
-  return (
-    <MorphingDialog
-      transition={{
-        type: 'spring',
-        bounce: 0,
-        duration: 0.3,
-      }}
-    >
-      <MorphingDialogTrigger>
-        <video
-          src={src}
-          autoPlay
-          loop
-          muted
-          className="aspect-video w-full cursor-zoom-in rounded-xl"
-        />
-      </MorphingDialogTrigger>
-      <MorphingDialogContainer>
-        <MorphingDialogContent className="relative aspect-video rounded-2xl bg-zinc-50 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-950 dark:ring-zinc-800/50">
-          <video
-            src={src}
-            autoPlay
-            loop
-            muted
-            className="aspect-video h-[50vh] w-full rounded-xl md:h-[70vh]"
-          />
-        </MorphingDialogContent>
-        <MorphingDialogClose
-          className="fixed top-6 right-6 h-fit w-fit rounded-full bg-white p-1"
-          variants={{
-            initial: { opacity: 0 },
-            animate: {
-              opacity: 1,
-              transition: { delay: 0.3, duration: 0.1 },
-            },
-            exit: { opacity: 0, transition: { duration: 0 } },
-          }}
-        >
-          <XIcon className="h-5 w-5 text-zinc-500" />
-        </MorphingDialogClose>
-      </MorphingDialogContainer>
-    </MorphingDialog>
-  )
+type ProgressiveBlurHoverProps = {
+  url: string
+  name: string
+  link: string
 }
+
+export function ProgressiveBlurHover({ url, name, link }: ProgressiveBlurHoverProps) {
+  const [isHover, setIsHover] = useState(false);
+
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className='block relative my-4 aspect-square h-[300px] overflow-hidden rounded-[4px]'
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <img
+        src={url}
+        alt={name}
+        className='absolute inset-0 w-full h-full object-cover object-top'
+      />
+      <ProgressiveBlur
+        className='pointer-events-none absolute bottom-0 left-0 h-[75%] w-full'
+        blurIntensity={0.5}
+        animate={isHover ? 'visible' : 'hidden'}
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+        transition={{ duration: 0.2, ease: 'easeOut' }}
+      />
+      <motion.div
+        className='absolute bottom-0 left-0'
+        animate={isHover ? 'visible' : 'hidden'}
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+        transition={{ duration: 0.2, ease: 'easeOut' }}
+      >
+        <div className='flex flex-col items-start gap-0 px-5 py-4'>
+          <p className='text-base font-medium text-white'>{name}</p>
+        </div>
+      </motion.div>
+    </a>
+  );
+}
+
+
 
 function MagneticSocialLink({
   children,
@@ -137,8 +146,8 @@ export default function Personal() {
       >
         <div className="flex-1">
           <p className="text-zinc-600 dark:text-zinc-400">
-            Focused on creating intuitive and performant web experiences.
-            Bridging the gap between design and development.
+            Curious child turned mobile developer — still tinkering, just with pixels and code.
+            Designing journeys, building apps, chasing that 'aha!' moment.
           </p>
         </div>
       </motion.section>
@@ -147,13 +156,11 @@ export default function Personal() {
         variants={VARIANTS_SECTION}
         transition={TRANSITION_SECTION}
       >
-        <h3 className="mb-5 text-lg font-medium">Selected Projects</h3>
+        <h3 className="mb-5 text-lg font-medium">Projects</h3>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
           {PROJECTS.map((project) => (
             <div key={project.name} className="space-y-2">
-              <div className="relative rounded-2xl bg-zinc-50/40 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-950/40 dark:ring-zinc-800/50">
-                <ProjectVideo src={project.video} />
-              </div>
+                <ProgressiveBlurHover url={project.url} name={project.name} link={project.link} />
               <div className="px-1">
                 <a
                   className="font-base group relative inline-block font-[450] text-zinc-900 dark:text-zinc-50"
