5074f67bba203f6c77c5e580c04e1405d7e85086
Robin <emeka14real@gmail.com>
2025-10-06 03:39:14 +0100

    Complete project updates

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,34 +1,89 @@
-import Link from "next/link";
-import type { Metadata } from "next";
-import { projects } from "./project-data";
-import { getProjectPosts } from "@/app/lib/posts";
+import Link from 'next/link'
+import type { Metadata } from 'next'
+import { projects, personalProjects } from './project-data'
+import { getProjectPosts } from '@/app/lib/posts'
+import { Tilt } from '@/components/motion-primitives/tilt'
 
 export const metadata: Metadata = {
-  title: "Projects",
-  description: "Nextfolio Projects",
-};
+  title: 'Projects',
+  description: 'Nextfolio Projects',
+}
 
 export default function Projects() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-medium">Projects</h1>
-      <div>
-        {projects.map((project, index) => (
-          <Link
-            key={index}
-            href={project.url}
-            className="flex flex-col space-y-1 mb-5 transition-opacity duration-200 hover:opacity-80"
-            rel="noopener noreferrer">
+    <div className="space-y-24">
+      <PersonalProjects/>
+      <CompanyProjects/>
+    </div>
+  )
+}
 
-            <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
-              <h2 className="text-black dark:text-white">{project.title}</h2>
-              <p className="text-neutral-600 dark:text-neutral-400">
-                {project.description}
-              </p>
-            </div>
-          </Link>
+function PersonalProjects() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-medium">Personal Projects & Case studies</h1>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+        {personalProjects.map((project, index) => (
+          <TiltCard
+            key={index}
+            image={project.image}
+            title={project.title}
+            description={project.description}
+            url={project.url}
+            target={project.target}
+          />
         ))}
       </div>
     </section>
-  );
+  )
+}
+
+function CompanyProjects() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-medium">Company Projects</h1>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+        {projects.map((project, index) => (
+          <TiltCard
+            key={index}
+            image={project.image}
+            title={project.title}
+            description={project.description}
+            url={project.url}
+            target={project.target}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+type TiltCardProps = {
+  image: string
+  title: string
+  description: string
+  url: string
+  target?: '_blank' | '_self'
+}
+
+function TiltCard({ image, title, description, url, target = '_blank' }: TiltCardProps) {
+  return (
+    <Link href={url} target={target} className="cursor-pointer">
+      <Tilt rotationFactor={8} isRevese>
+        <div
+          style={{
+            borderRadius: '12px',
+          }}
+          className="flex max-w-[270px] flex-col overflow-hidden border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900">
+          <img src={image} alt={title} className="h-48 w-full object-cover" />
+          <div className="p-2">
+            <h1 className="font-mono leading-snug text-zinc-950 dark:text-zinc-50">
+              {title}
+            </h1>
+            <p className="text-zinc-700 dark:text-zinc-400">{description}</p>
+          </div>
+        </div>
+      </Tilt>
+    </Link>
+  )
 }
--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -3,6 +3,8 @@ export interface Project {
   year: number;
   description: string;
   url: string;
+  image: string;
+  target?: '_blank' | '_self';
 }
 
 export const projects: Project[] = [
@@ -11,11 +13,26 @@ export const projects: Project[] = [
     year: 2025,
     description: "Fintech",
     url: "https://play.google.com/store/apps/details?id=co.grey.mobile.android&hl=en&pli=1",
+    image: "https://cdn.prod.website-files.com/6360022338a81bd6fdbb1145/6563c15e45db91a91be06021_card%203%20Img.svg",
+    target: "_self"
   },
   {
     title: "JustEase",
     year: 2023,
     description: "Lawtech",
     url: "https://play.google.com/store/apps/details?id=com.lawpavilion.lr&hl=en",
+    image: "https://lawrights.lawpavilion.com/images/phone.png",
+    target: "_self"
+  },
+];
+
+export const personalProjects: Project[] = [
+  {
+    title: "Star Wars",
+    year: 2025,
+    description: "[ 🚧 Work in progress ]",
+    url: "https://github.com/codejunk1e/StarWars",
+    image: "https://raw.githubusercontent.com/codejunk1e/StarWars/refs/heads/master/images/collapsing_menu.png",
+    target: "_self"
   },
 ];
